96d37281b5325b0a70f938e6410d68d25f031099
Majid Azizian <azizian.majid@gmail.com>
2020-12-16 19:30:16 +0330

    mongo repository  and json serializer

--- go.mod
+++ go.mod
@@ -5,5 +5,9 @@ go 1.15
 require (
 	github.com/pkg/errors v0.9.1
 	github.com/teris-io/shortid v0.0.0-20201117134242-e59966efd125
+	github.com/vmihailenco/msgpack v4.0.4+incompatible
+	go.mongodb.org/mongo-driver v1.4.4
+	google.golang.org/appengine v1.6.7 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/dealancer/validate.v2 v2.1.0
 )
---
+++ repository/mongo/repository.go
@@ -0,0 +1,33 @@
+package mongo
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"time"
+)
+
+type mongoRepository struct {
+	client		*mongo.Client
+	database	string
+	timeout		time.Duration
+}
+
+func newMongoClient(mongoURL string, mongoTimeout int) (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(mongoTimeout)*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURL))
+	if err != nil {
+		return nil, err
+	}
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, err
+	}
+	return client, err
+}
+
+//6:22
+//https://www.youtube.com/watch?v=xUYDkiPdfWs&list=PLJbE2Yu2zumAixEws7gtptADSLmZ_pscP&index=6
